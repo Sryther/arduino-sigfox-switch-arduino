8ce7c9854125b211fa47f4b63089734d197193d4
Paul Rey <paul.rey@sryther.fr>
2017-12-23 23:11:55 +0100

    Add init scripts

---
+++ init/blinkLedTest/blinkLedTest.ino
@@ -0,0 +1,15 @@
+int ledPin = A0;
+int potPin = A1;
+int val = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  val = analogRead(potPin);
+  Serial.println(val);
+  delay(300);
+}
